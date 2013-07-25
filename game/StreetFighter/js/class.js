6a3524a622d85519050c70f0bcb61075334ffe6b
ijibu <ijibu.com@gmail.com>
2013-07-25 20:23:22 +0800

    重新提交街霸游戏代码 Signed-off-by: ijibu <ijibu.com@gmail.com>

--- game/StreetFighter/js/class.js
+++ game/StreetFighter/js/class.js
@@ -1,45 +1,45 @@
-var Class = function () {
+
+
+
+var Class = function(){
+
 	var cache = {};
 
-	var create = function (fn, methods, parent) {
-		var _initialize,
-		_instances = [],
-		instance,
-		_unique = 0;
+	var create = function( fn, methods, parent ){
 
-		_initialize = function (args) {
-			fn.apply(this, args);
+		var _initialize, _instances = [], instance, _unique = 0;
+
+		_initialize = function( args ){
+			fn.apply( this, args );
 		}
 
-		if (parent) {
+		if ( parent ){
 			_initialize.prototype = parent;
 		}
 
-		for (var i in methods) {
-			_initialize.prototype[i] = methods[i];
+		for ( var i in methods ){
+			_initialize.prototype[ i ] = 	methods[ i ];
 		}
 
-		_initialize.prototype.implement = function () {
-			var fns = arguments[0].split('.'),
-			args = Array.prototype.slice.call(arguments, 1),
-			fn = this;
-			for (var i = 0, c; c = fns[i++]; ) {
-				fn = fn[c];
-				if (!fn) {
-					return alert('接口尚未实现');
+		_initialize.prototype.implement = function(){
+			var fns = arguments[0].split('.'), args = Array.prototype.slice.call( arguments, 1 ), fn = this;
+			for ( var i = 0, c; c = fns[i++]; ){
+				fn = fn[ c ];
+				if ( !fn ){
+					return alert ('接口尚未实现');
 				}
 			}
-			return fn.apply(this, args);
+			return fn.apply( this, args );
 		}
 
-		var getInstance = function () {
-			var args = Array.prototype.slice.call(arguments, 0);
-			_instances[_unique++] = new _initialize(args);
-			return _instances[_unique - 1];
+		var getInstance = function(){
+			var args = Array.prototype.slice.call( arguments, 0 );
+			 _instances[ _unique++ ] = new _initialize( args );
+			return _instances[ _unique - 1 ];
 		}
 
-		var empty = function () {
-			for (var i = 0, c; c = _instances[i++]; ) {
+		var empty = function(){
+			for ( var i = 0, c; c = _instances[i++]; ){
 				c = null;
 			}
 			_instances = [];
@@ -47,51 +47,64 @@ var Class = function () {
 			_unique = 0;
 		}
 
-		var getCount = function () {
+		var getCount = function(){
 			return _unique;
 		}
 
-		var getPrototype = function () {
+		var getPrototype = function(){
 			return _initialize.prototype;
 		}
 
-		var subClass = function (fn, methods) {
-			var a = Class.create(fn, methods, _instances[0] || getInstance());
+		var subClass = function( fn, methods ){
+			var a = Class.create( fn, methods, _instances[0] || getInstance() );
 			return a
 		}
 
-		var interface = function (key, fn) {
-			if (_initialize) {
-				_initialize.prototype[key] = fn;
+		var interface = function( key, fn ){
+			if ( _initialize ){
+				_initialize.prototype[ key ] = fn;
 			}
 		}
 
 		return {
-			interface : interface,
-			getInstance : getInstance,
-			empty : empty,
-			getCount : getCount,
-			getPrototype : getPrototype,
-			subClass : subClass,
+			interface: interface,
+			getInstance: getInstance,
+			empty: empty,
+			getCount: getCount,
+			getPrototype: getPrototype,
+			subClass: subClass,
 		}
+
 	}
 
 	return {
-		create : create,
-		getInstances : function () {
-			return _instances;
+		create: create,
+		getInstances: function(){
+			return _instances;	
 		}
 	}
-}
-()
+	
+	
+}()
+
+
+
+
+
 
 /*
+
 Sven = Canvas.subClass( function( a, b, c ){
-this.age = a;
+	this.age = a;
 });
 
+
 Sven.addMethod( 'add1', function(){
-return 'haha';
+	return 'haha';
 })
 
- */
+*/
+
+
+
+
