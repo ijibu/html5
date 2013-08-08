02d3ef5ff1e8511d497fc065d27528e8556d8908
work <ijibu.com@gmail.com>
2013-08-08 17:52:50 +0800

    提交桌球游戏使用说明 Signed-off-by: work <ijibu.com@gmail.com>

--- game/WebSnooker/server.php
+++ game/WebSnooker/server.php
@@ -1,3 +1,8 @@
 <?php
 $ret = array("status" => 1, "received" => 1, "sent" => 1);
 echo json_encode($ret);exit;
+
+/**
+ * Ë«ÈËÓÎÏ·¡£
+ * {"status":1,"received":1,"ack":"1375954421","packets":[{"time":"1375954421","data":{"event":"shoot","x":"0.5622127497947318","y":"-0.15236583786410068","player":"52685f","hash":"3aab71d78143afd37916930094901368"}}]}
+ */
